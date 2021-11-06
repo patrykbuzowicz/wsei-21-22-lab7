2dc013c3ef37165c252a7f1f2e483e32827086dd
Patryk Buzowicz <patryk_buzowicz@hotmail.com>
2021-11-06 16:11:55 +0100

    save to database

--- Wsei.Lab7/Controllers/ProductsController.cs
+++ Wsei.Lab7/Controllers/ProductsController.cs
@@ -3,19 +3,38 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Wsei.Lab7.Database;
+using Wsei.Lab7.Entities;
 using Wsei.Lab7.Models;
 
 namespace Wsei.Lab7.Controllers
 {
     public class ProductsController : Controller
     {
+        private readonly AppDbContext _dbContext;
+
+        public ProductsController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Add(ProductModel product)
+        public async Task<IActionResult> Add(ProductModel product)
         {
+            var entity = new ProductEntity
+            {
+                Name = product.Name,
+                Description = product.Description,
+                IsVisible = product.IsVisible,
+            };
+
+            await _dbContext.Products.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+
             var viewModel = new ProductStatsViewModel
             {
                 NameLength = product.Name.Length,
---
+++ Wsei.Lab7/Entities/ProductEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Wsei.Lab7.Entities
+{
+    public class ProductEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsVisible { get; set; }
+    }
+}
